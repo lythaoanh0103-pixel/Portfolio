343a1dffc4ad4867a5601e733da6aa479e46feaf
unknown <lythaoanh0103@gmail.com>
2026-08-04 14:50:55 +0700

    Remove GitHub Pages config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const isGithubPages = process.env.GITHUB_ACTIONS || process.env.NODE_ENV === "production"
+const isGithubPages = process.env.GITHUB_ACTIONS === "true"
 
 const nextConfig = {
 
